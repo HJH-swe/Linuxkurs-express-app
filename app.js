bc1c8d978d4587692459f9b27cb67216c21b7c94
Hannah J-H <h.jerminghavill@gmail.com>
2026-02-23 15:20:05 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello from my Express.js app!</h1>
+<p>P.S. Cats rule</p>`);
+});
+
+app.listen(port, () => {
+  console.log(`App is running at http://localhost:${port}`);
+});
